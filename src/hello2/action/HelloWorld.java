f67b8000d6cb7729459de298631dbbd42a7bd080
user2 <user2@KH2>
2016-10-10 20:42:48 +0900

    .

---
+++ src/hello2/action/HelloWorld.java
@@ -0,0 +1,13 @@
+package hello2.action;
+
+import com.opensymphony.xwork2.Action;
+
+public class HelloWorld implements Action {
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return SUCCESS;
+	}
+
+}
